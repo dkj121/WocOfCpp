9fe4f008cb8096faa3cb3091d2343b167505553a
毅 王 <2726073422@qq.com>
2026-02-16 13:19:56 +0800

    start

---
+++ ElementaryPart/build/CMakeFiles/JsonExpression.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JsonExpression.dir/JsonExpression.cpp.obj"
+  "CMakeFiles/JsonExpression.dir/JsonExpression.cpp.obj.d"
+  "libJsonExpression.a"
+  "libJsonExpression.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JsonExpression.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ElementaryPart/build/CMakeFiles/JsonExpression.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libJsonExpression.a"
+)
