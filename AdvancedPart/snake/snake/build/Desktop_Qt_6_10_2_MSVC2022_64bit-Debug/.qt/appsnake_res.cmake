7e5b810d30f1a1345009e8c7b14d486728a2d3e1
毅 王 <2726073422@qq.com>
2026-02-24 20:53:17 +0800

    start snake with Qt

---
+++ AdvancedPart/snake/snake/build/Desktop_Qt_6_10_2_MSVC2022_64bit-Debug/.qt/appsnake_res.cmake
@@ -0,0 +1,7 @@
+
+set(target "appsnake")
+set(working_dir "E:/Code_Programs/learning/CorCppLearning/WocOfCpp/AdvancedPart/snake/snake")
+set(src_and_dest_list
+
+)
+set(timestamp_file "E:/Code_Programs/learning/CorCppLearning/WocOfCpp/AdvancedPart/snake/snake/build/Desktop_Qt_6_10_2_MSVC2022_64bit-Debug/.qt/appsnake_res.txt")
