7e7e21ba5cc576a3b9a05726c07a7851c765e02c
毅 王 <2726073422@qq.com>
2026-02-28 20:32:56 +0800

    re

--- AdvancedPart/snake/snake/build/Desktop_Qt_6_10_2_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_appsnake_FILE E:/Code_Programs/learning/CorCppLearning/WocOfCpp/AdvancedPart/snake/snake/build/Desktop_Qt_6_10_2_MSVC2022_64bit-Debug/appsnake.exe)
-set(__QT_DEPLOY_TARGET_appsnake_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_appsnake_RUNTIME_DLLS E:/Qt/6.10.2/msvc2022_64/bin/Qt6Quickd.dll;E:/Qt/6.10.2/msvc2022_64/bin/Qt6OpenGLd.dll;E:/Qt/6.10.2/msvc2022_64/bin/Qt6Guid.dll;E:/Qt/6.10.2/msvc2022_64/bin/Qt6QmlMetad.dll;E:/Qt/6.10.2/msvc2022_64/bin/Qt6QmlModelsd.dll;E:/Qt/6.10.2/msvc2022_64/bin/Qt6QmlWorkerScriptd.dll;E:/Qt/6.10.2/msvc2022_64/bin/Qt6Qmld.dll;E:/Qt/6.10.2/msvc2022_64/bin/Qt6Networkd.dll;E:/Qt/6.10.2/msvc2022_64/bin/Qt6Cored.dll)
--- AdvancedPart/snake/snake/build/Desktop_Qt_6_10_2_MSVC2022_64bit-Debug/.qt/appsnake_qml.cmake
+++
@@ -1,9 +0,0 @@
-
-set(target "appsnake")
-set(working_dir "E:/Code_Programs/learning/CorCppLearning/WocOfCpp/AdvancedPart/snake/snake")
-set(src_and_dest_list
-    "E:/Code_Programs/learning/CorCppLearning/WocOfCpp/AdvancedPart/snake/snake/Main.qml"
-    "E:/Code_Programs/learning/CorCppLearning/WocOfCpp/AdvancedPart/snake/snake/build/Desktop_Qt_6_10_2_MSVC2022_64bit-Debug/snake/Main.qml"
-
-)
-set(timestamp_file "E:/Code_Programs/learning/CorCppLearning/WocOfCpp/AdvancedPart/snake/snake/build/Desktop_Qt_6_10_2_MSVC2022_64bit-Debug/.qt/appsnake_qml.txt")
